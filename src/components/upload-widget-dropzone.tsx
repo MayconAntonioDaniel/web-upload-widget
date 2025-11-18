22907b38b8aa776d03bbcd88f72225696dcd70e4
Maycon Antônio Daniel <mayconantonio1997@hotmail.com>
2025-11-18 14:56:15 -0300

    Estrutura do Widget

---
+++ src/components/upload-widget-dropzone.tsx
@@ -0,0 +1,7 @@
+export function UploadWidgetDropzone() {
+  return (
+    <div className="px-2 h-32">
+
+    </div>
+  )
+}
---
+++ src/components/upload-widget-upload-list.tsx
@@ -0,0 +1,7 @@
+export function UploadWidgetUploadList() {
+  return (
+    <div className="px-2 h-48">
+
+    </div>
+  )
+}
