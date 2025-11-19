31fd875f7324509dca6958d377a5ab65e5e391d9
Maycon Antônio Daniel <mayconantonio1997@hotmail.com>
2025-11-19 10:42:28 -0300

    Estados Visuais nos Componentes

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,5 +1,20 @@
+import { UploadCloud } from "lucide-react"
+
 export function UploadWidgetTitle() {
+  const isThereAnyPendingUpload = true
+  const uploadGlobalPorcentage = 66
+
   return (
-    <span className="text-sm font-medium">Upload files</span>
+    <div className="flex items-center gap-1.5 text-sm font-medium">
+      <UploadCloud className="size-4 text-zinc-400" strokeWidth={ 1.5 } />
+        { isThereAnyPendingUpload ? (
+          <span className="flex items-baseline gap-1">
+            Uploading
+            <span className="text-xs text-zinc-400 tabular-nums">{uploadGlobalPorcentage}%</span>
+          </span>
+        ) : (
+          <span className="text-sm font-medium">Upload files</span>
+        )}
+    </div>
   )
 }
