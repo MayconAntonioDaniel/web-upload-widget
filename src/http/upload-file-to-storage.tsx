95d637eeab08142267ae79949f7a0535a4a7f5d9
Maycon Antônio Daniel <mayconantonio1997@hotmail.com>
2025-11-21 10:32:46 -0300

    Cancelamento e retentativa

--- src/http/upload-file-to-storage.tsx
+++ src/http/upload-file-to-storage.tsx
@@ -4,15 +4,25 @@ interface UplaodFileToStorageParams {
   file: File
 }
 
-export async function uploadFileToStorage({ file }: UplaodFileToStorageParams) {
+interface UplaodFileToStorageOptions {
+  signal?: AbortSignal
+}
+
+export async function uploadFileToStorage(
+  { file }: UplaodFileToStorageParams,
+  options?: UplaodFileToStorageOptions
+) {
   const data = new FormData()
 
   data.append('file', file)
 
+  throw new Error()
+
   const reponse = await axios.post<{ url: string }>('http://localhost:3333/uploads', data, {
     headers: {
       'Content-Type': 'multipart/form-data',
-    }
+    },
+    signal: options?.signal,
   })
 
   return { url: reponse.data.url }
